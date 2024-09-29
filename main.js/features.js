82f26ba4e899b41cc28380934dd801a383767a48
Guuhhfetf <sukhi.law.uap@gmail.com>
2024-09-29 22:50:58 +0600

    added a features

---
+++ main.js/features.js
@@ -0,0 +1,9 @@
+document.getElementById('show-add-money-form')
+.addEventListener('click',function(){
+       showSectionById('add-money-form');                 
+})
+
+document.getElementById('show-transaction-history')
+.addEventListener('click', function(){
+   showSectionById('transaction-section');     
+})
--- main.js/home.js
+++ main.js/home.js
@@ -2,3 +2,4 @@ let btnClick = document.querySelector ("button");
 btnClick.addEventListener ("click",()=>{
 window.location.href="page2.html";
 });
+
